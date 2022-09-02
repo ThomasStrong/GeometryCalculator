8534bb5d35fe11852ed4355d9017c80ceee2f441
Tom Strong <strng_thms@yahoo.com>
2022-09-01 21:51:52 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Geometry_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
